c04efbf655c272854f186edaf591a01032f521ed
Imam Hosen Emon <86104376+imam-xs@users.noreply.github.com>
2026-01-20 00:43:26 +0600

    added explanation

--- first-program/main.go
+++ first-program/main.go
@@ -1,6 +1,6 @@
-package main
-import "fmt"
+package main  //every Go program belongs to a package. without package main, the program would not start.
+import "fmt" //the fmt package is part of Go’s standard library and is used for formatting input and output
 
-func main(){
+func main(){  //a Go program execution always starts from main() function
 	fmt.Println("Hello World")
 }
