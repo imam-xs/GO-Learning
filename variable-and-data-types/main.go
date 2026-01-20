78df386f243575416e84fcec62cc73313441ba86
Imam Hosen Emon <86104376+imam-xs@users.noreply.github.com>
2026-01-20 23:07:31 +0600

    basic data type and ways to declare variable

--- variable-and-data-types/main.go
+++ variable-and-data-types/main.go
@@ -3,7 +3,27 @@ package main
 import "fmt"
 
 func main()  {
-	a := 10
-	fmt.Println(a)
+	//explicit declaration (var)
+	var age int = 30 //integer
+	fmt.Println(age)
+
+	var personName string = "Jon doe" // string
+	fmt.Println(personName)
+
+	var name = "Emon" //type inferred as string
+	fmt.Println(name)
+
+	var f float64 = 30.25 //floating number
+	fmt.Println(f)
+
+	var bol bool = true //boolean
+	fmt.Println(bol)
+
+   //short declaration (:=)
+    clas := 10
+	fmt.Println(clas)
+
+	subject := "English"
+	fmt.Println(subject)
 	
 }
