f0ab3830d41349419d2e9a94b2d4a6eba6822d07
Imam Hosen Emon <86104376+imam-xs@users.noreply.github.com>
2026-02-05 23:52:42 +0600

    Add example standard function
    Add functions/standard-function/standard.go demonstrating a simple standardFunction that prints a message using fmt. Includes a main that calls standardFunction with an example message to show function definition and invocation.

---
+++ functions/standard-function/standard.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func standardFunction(message string) {
+	fmt.Println(message)
+}
+
+func main() {
+	standardFunction("This is an example of a standard function.")
+}
